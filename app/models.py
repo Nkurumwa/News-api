21394cab9edf1bd3c4a2cbd093c2a45c1fbb0532
Nkurumwa <nkurumwa51@gmail.com>
2019-10-14 11:59:28 +0300

    adds classes

---
+++ app/models.py
@@ -0,0 +1,23 @@
+import datetime
+
+class NewsSource:
+    def __init__(self,id,name,url,description,category,language,country):
+        '''This function creates an object for the news sources'''
+        self.id= id
+        self.name = name
+        self.url = url
+        self.description = description
+        self.category = category.capitalize()
+        self.language = language
+        self.country = country
+
+class NewsArticle:
+    def __init__(self,author,title,description,url,image,published,content):
+        ''' This function creates an instance of a source news '''
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.published = published
+        self.content = content
