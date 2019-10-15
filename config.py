6a35715ec0815c3104eec18dabd83c85336ae3a8
Nkurumwa <nkurumwa51@gmail.com>
2019-10-15 15:53:21 +0300

    adds articles base  url

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
- import os
+
+import os
 
 class Config:
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    NEWS_API_SOURCES_URL = 'https://newsapi.org/v2/sources?language=&country={}&apiKey={}'
+    ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    # NEWS_API_KEY_URL='https://newsapi.org/v2/sources?apiKey={}'
+    
+   
 
 class ProdConfig(Config):
     pass
